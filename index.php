ee29435f9aea9ebd317c690781b9b2595daa1670
Marco Macelan <marcomacelan@yahoo.com.br>
2022-11-17 15:41:13 -0300

    Aula 2

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Danki Code - Tema</title>
+	<title>Macelan - Tema</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
@@ -13,19 +13,29 @@
 	<section class="topo">
 		<div class="center">
 			<header>
-				<div class="logo"><img src="images/logo.png" /></div><!--logo-->
+				<div class="logo"><img src="images/logomeu.png" /></div><!--logo-->
 			</header>
 			<ul class="menu-desktop">
 				<li><a href="">Home</a></li>
+				<li><a href="">Notícias</a></li>
+				<li><a href="">Polícia</a></li>
+				<li><a href="">Educação</a></li>
+				<li><a href="">Igrejas</a></li>
+				<li><a href="">Intreterimento</a></li>
 				<li><a href="">Sobre</a></li>
 				<li><a href="">Contato</a></li>
 			</ul>
 			<div class="menu-mobile">
 			<i class="fas fa-align-right"></i>
 				<ul>
-					<li><a href="">Home</a></li>
-					<li><a href="">Sobre</a></li>
-					<li><a href="">Contato</a></li>
+				<li><a href="">Home</a></li>
+				<li><a href="">Notícias</a></li>
+				<li><a href="">Polícia</a></li>
+				<li><a href="">Educação</a></li>
+				<li><a href="">Igrejas</a></li>
+				<li><a href="">Intreterimento</a></li>
+				<li><a href="">Sobre</a></li>
+				<li><a href="">Contato</a></li>
 				</ul>
 			</div><!--menu-mobile-->
 			<div class="clear"></div>
